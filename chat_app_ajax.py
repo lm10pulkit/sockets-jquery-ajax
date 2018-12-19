08560550ba613e9aa181517ed81663cabbdfecdf
realmadr4pulkit@gmail.com <root@localhost.localdomain>
2018-12-19 19:29:08 +0530

    basic done

--- chat_app_ajax.py
+++ chat_app_ajax.py
@@ -35,7 +35,7 @@ def signup():
         mysql.connection.commit()
         cur.close()
         session['user']=username
-        return redirect(url_for('protected'))
+        return redirect(url_for('chatoptions'))
     else:
         return 'get'
 
@@ -58,7 +58,7 @@ def login():
     else:
         return 'password did not match!'
 
-    return 'u are logged in!'
+    return redirect(url_for('chatoptions'))
 
 
 @app.route('/protected')
@@ -82,8 +82,49 @@ def chatoptions():
         resultValue = cur.execute(" select * from users where username <>'%s' " %(username))
         if resultValue>0:
             options=cur.fetchall()
-
             return render_template('chat_options.html',options=options)
+        else:
+            'there is no one to chat'
+    return redirect(url_for('index'))
+
+@app.route('/chat/<r_username>', methods=['GET','POST'])
+def chat(r_username=None):
+    if 'user' not in session:
+        return redirect(url_for('index'))
+    reciever = str(r_username)
+    sender = str(session['user'])
+    cur = mysql.connection.cursor()
+    if request.method=='GET':
+        resultValue = cur.execute(" select * from users where username ='%s' " % (r_username))
+        if 'user' in session:
+            if resultValue ==0:
+                return '<h2>No user of this username registered</h2>'
+            elif r_username==session['user']:
+                return '<h2>Come on dawg! u cant send urself a message</h2>'
+            else:
+                dict={}
+                dict['reciever']=str(r_username)
+
+                select_stmt = ("select * from messages where (sender = (%s) and reciever=(%s)) or (sender = (%s) and reciever=(%s))")
+                data =(sender,reciever,reciever,sender)
+                cur.execute(select_stmt,data)
+                dict['messages']=cur.fetchall()
+                print(dict['messages'])
+                return render_template('chat.html',Details=dict)
+        else:
+            return redirect(url_for('index'))
+    else:
+        if 'user' in session:
+            message = str(request.form['message'])
+            cur.execute("INSERT INTO messages (sender,reciever,message) values (%s,%s,%s)" ,(sender,reciever,message))
+            mysql.connection.commit()
+            cur.close()
+            return redirect(url_for('chat',r_username=reciever))
+        else:
+            return redirect(url_for('index'))
+
+
+
 
 if(__name__=='__main__'):
     app.run(debug=True)
